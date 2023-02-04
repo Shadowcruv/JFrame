639c95227380954261b0d21ef3e8182fd46130b5
miracleihediwa <miracleihediwa619@gmail.com>
2023-02-04 20:12:20 +0100

    upload files

---
+++ src/House.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+
+public class House {
+    public static void main(String[] args) {
+        JFrame myWindow;  // Object Declaration
+        myWindow = new JFrame(); //Object Creation
+        myWindow.setSize(300, 200);
+        myWindow.setTitle("Uchechi Oluebube Michael");
+        myWindow.setVisible(true);
+    }
+
+
+
+}
